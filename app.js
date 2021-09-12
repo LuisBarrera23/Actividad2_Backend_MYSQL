55a7885943e6b7ee047e1e3676e31e0139d877d1
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2021-09-11 19:29:16 -0600

    configuracion inicial

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser');
+const PORT=process.env.PORT || 5000;
+
+const app = express();
+
+app.use(bodyParser().json());
+
+// MySql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'rootpass',
+    database: 'node20_mysql'
+  });
