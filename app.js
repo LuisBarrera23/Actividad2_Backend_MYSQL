310d9bc28e4dbcd0a431326082d57e48629b8c99
LuisBarrera23 <78067073+LuisBarrera23@users.noreply.github.com>
2021-09-25 20:59:20 -0600

    modificacion

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var cors = require('cors')
 const mysql = require('mysql');
 
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors())
